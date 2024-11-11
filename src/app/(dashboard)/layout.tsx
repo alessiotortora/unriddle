5fdb9fa1919c2d4dd5f701b0d28ef03398bc056e
Alessio <alessiotortora@gmail.com>
2024-11-11 21:48:30 +0100

    fix bug sidebar and added new items to sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,13 @@
-import { AppSidebar } from '@/components/layout/app-sidebar';
+import { AppSidebar } from '@/components/layout/sidebar/app-sidebar';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 interface DashboardLayoutProps {
@@ -12,7 +21,23 @@ export default async function DashboardLayout({
     <SidebarProvider defaultOpen={true}>
       <AppSidebar />
       <main>
-        <SidebarTrigger />
+        <header className="flex h-16 w-screen shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
         {children}
       </main>
     </SidebarProvider>
---
+++ src/components/layout/sidebar/app-sidebar.tsx
@@ -0,0 +1,119 @@
+import Link from 'next/link';
+
+import {
+  Calendar,
+  GalleryVerticalEnd,
+  Home,
+  Inbox,
+  LifeBuoy,
+  MoreHorizontal,
+  Plus,
+  Search,
+  Send,
+  Settings,
+} from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+import { NavSecondary } from './nav-secondary';
+import { NavUser } from './nav-user';
+
+const items = [
+  { title: 'Home', url: '/dashboard', icon: Home },
+  { title: 'Inbox', url: '/store', icon: Inbox },
+  { title: 'Calendar', url: '#', icon: Calendar },
+  { title: 'Search', url: '#', icon: Search },
+  { title: 'Settings', url: '#', icon: Settings },
+];
+
+const navSecondary = [
+  {
+    title: 'Support',
+    url: '#',
+    icon: LifeBuoy,
+  },
+  {
+    title: 'Feedback',
+    url: '#',
+    icon: Send,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon" side="left" variant="sidebar">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-4" />
+                </div>
+                <div className="flex flex-col gap-0.5 leading-none">
+                  <span className="font-semibold">Documentation</span>
+                  <span className="">v1.0.0</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Navigation</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                  <SidebarMenuBadge>20</SidebarMenuBadge>
+                </SidebarMenuItem>
+              ))}
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <MoreHorizontal />
+                  <span>More</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupAction title="Add project">
+            <Plus /> <span className="sr-only">Add Project</span>
+          </SidebarGroupAction>
+        </SidebarGroup>
+        <NavSecondary items={navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser
+          user={{
+            name: 'alessio',
+            email: 'alessiotortora@gmail.com',
+            avatar: '',
+          }}
+        />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
